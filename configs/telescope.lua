b21fbd8888e7566ab0ff3adcd143dad2c55a8eba
gorjay <gorjay.huang@gmail.com>
2024-05-04 20:02:03 +0800

    feat: added smart-column

--- configs/telescope.lua
+++ configs/telescope.lua
@@ -1,5 +1,6 @@
 return {
 	defaults = {
+		path_display = "smart",
 		history = {
 			path = "~/.local/share/nvim/databases/telescope_history.sqlite3",
 			limit = 100,
--- plugins/editor.lua
+++ plugins/editor.lua
@@ -1,15 +1,20 @@
 local custom = {}
 
-custom["folke/todo-comments.nvim"] = {
-	lazy = true,
-	event = "BufRead",
-	config = function() require("todo-comments").setup() end,
-}
-
 custom["kylechui/nvim-surround"] = {
 	lazy = true,
 	event = "VeryLazy",
 	config = function() require("nvim-surround").setup({}) end,
 }
+
+custom["m4xshen/smartcolumn.nvim"] = {
+	lazy = true,
+	event = "BufReadPost",
+}
+
+-- FIXME: vim-sneak's semicolon doesn't work
+-- custom["justinmk/vim-sneak"] = {
+-- 	lazy = true,
+-- 	event = "BufReadPost",
+-- }
 
 return custom
