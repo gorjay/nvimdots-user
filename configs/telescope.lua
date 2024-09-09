a8274c77375052f6d6a956aeab80a7c215cdd06d
gorjay <gorjay.huang@gmail.com>
2024-09-09 21:47:23 +0800

    tool: telescope path_display set filename_first

--- configs/telescope.lua
+++ configs/telescope.lua
@@ -1,12 +1,15 @@
 return {
 	defaults = {
+		path_display = {
+			"filename_first",
+		},
 		history = {
 			path = "~/.local/share/nvim/databases/telescope_history.sqlite3",
 			limit = 100,
 		},
 		mappings = {
 			n = {
-				["q"] = "close"
+				["q"] = "close",
 			},
 		},
 	},
